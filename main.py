70834e22138bc2cd726c7f4c3ac2ac9f7bc940a6
Munaver-vp <85270013+Munaver-vp@users.noreply.github.com>
2021-06-03 09:33:39 +0530

    Create main.py

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ def BS():
     Robin = pyttsx3.init()
     voices = Robin.getProperty('voices')
     Robin.setProperty('voice', voices[1].id)
-    Robin.say("This Is a Munaver")
+    Robin.say("<Your Text>")
     Robin.runAndWait()
 
     wn = Tk()
-    wn.title("Mohammed Munaver Vp")
+    wn.title("<Your Text>")
     wn = Canvas(wn,width=632, height=851)
     wn.pack()
-    image = PhotoImage(file = 'kali.JPG')
+    image = PhotoImage(file = 'yourimage.jpg')
     wn.create_image(0,0, anchor=NW, image = image)
 
     wn.mainloop()
